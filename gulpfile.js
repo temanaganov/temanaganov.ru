7b9abb5364e2ac7b13841530b059f6e02827de20
Artem Naganov <artemnaganov10@gmail.com>
2021-03-07 00:14:47 +0300

    Update to the new template

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,10 @@ const postcss = require('gulp-postcss');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default;
-const plumber = require('gulp-plumber')
+const plumber = require('gulp-plumber');
 const del = require('del');
+
+const sassSyntax = 'scss';
 
 function browsersync() {
 	browserSync.init({
@@ -33,7 +35,7 @@ function html() {
 }
 
 function styles() {
-	return src('src/styles/**/*.scss')
+	return src(`src/styles/**/*.${sassSyntax}`)
 		.pipe(plumber())
 		.pipe(sass())
 		.pipe(postcss())
@@ -51,24 +53,28 @@ function scripts() {
 		.pipe(browserSync.stream());
 }
 
-function images() {
-	return src('src/images/**/*').pipe(dest('dist/images'));
+function copy() {
+	return src(['src/static/**/*', '!src/static/favicons/**/*'], { nodir: true }).pipe(dest('dist/static'));
 }
 
-function fonts() {
-	return src('src/fonts/*').pipe(dest('dist/fonts'));
+function favicons() {
+	return src('src/static/favicons/*').pipe(dest('dist'));
 }
 
 function clean() {
 	return del('dist', { force: true });
 }
 
+function readyReload(cb) {
+	browserSync.reload();
+	cb();
+}
+
 function startwatch() {
-	watch('src/pug/**/*.pug').on('change', series(html, browserSync.reload));
-	watch('src/styles/**/*.scss').on('change', styles);
-	watch('src/js/**/*.js').on('change', scripts);
-	watch('src/images/**/*').on('change', images);
-	watch('src/fonts/*').on('change', fonts);
+	watch('src/pug/**/*.pug', series(html, readyReload));
+	watch(`src/styles/**/*.${sassSyntax}`, styles);
+	watch('src/js/**/*.js', series(scripts, readyReload));
+	watch('src/static/**/*', series(copy, favicons, readyReload));
 }
 
 exports.browsersync = browsersync;
@@ -76,8 +82,9 @@ exports.startwatch = startwatch;
 exports.scripts = scripts;
 exports.styles = styles;
 exports.html = html;
-exports.fonts = fonts;
+exports.copy = copy;
+exports.favicons = favicons;
 exports.clean = clean;
 
-exports.build = series(clean, parallel(html, styles, scripts, images, fonts));
-exports.default = series(clean, parallel(html, styles, scripts, images, fonts), parallel(browsersync, startwatch));
+exports.build = series(clean, parallel(html, styles, scripts, copy, favicons));
+exports.default = series(clean, parallel(html, styles, scripts, copy, favicons), parallel(browsersync, startwatch));
